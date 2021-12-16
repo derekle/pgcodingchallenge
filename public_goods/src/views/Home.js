c7ee2d96088cb42c8e814671d35eb27c83fb288a
derek <derekle.creative@gmail.com>
2021-12-15 17:24:29 -0700

    code: render first text card

--- public_goods/src/views/Home.js
+++ public_goods/src/views/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ImageCard from '../components/imgCard';
+import TextCardA from '../components/txtCardA';
 const Home = (props) => {
     return (
         <div>
+            <TextCardA />
             <ImageCard imgsrc="assets/cat_image1_desktop.png" altlabel="Feeding Cat"/>
             <ImageCard imgsrc="assets/cat_image2_desktop.png" altlabel="Feeding Cat"/>
         </div>
