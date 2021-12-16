906061392c617792da2b149cb6a23903f4dd7660
derek <derekle.creative@gmail.com>
2021-12-15 17:24:19 -0700

    code: create First text card

---
+++ public_goods/src/components/txtCardA.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const TextCardA = (props) => {
+    return (
+        <div className='txtCardA'>
+            <h1>Treat your pet (and you).<br/>It's on us!</h1>
+            <p>Treat your pet to a healthy treat for <b>free</b> to kick off your Public Goods trial membership</p>
+            <b>See what it's like to be a Public Goods member.</b>
+            <li>Included in your bundle is a free 2-week membership that gives you unlimited access to our entire collection of sustainable essentials</li>
+            <li>Take a load off. We make it easy for you to make better choices. Always included: eco-friendly products you can trust.</li>
+        </div>
+
+    )
+}
+
+export default TextCardA
