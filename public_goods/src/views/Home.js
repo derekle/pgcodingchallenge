2bbc58eaf2fa8a239339b1324c1df6cf04d279c1
derek <derekle.creative@gmail.com>
2021-12-15 18:11:51 -0700

    code: create sections for components

--- public_goods/src/views/Home.js
+++ public_goods/src/views/Home.js
@@ -3,14 +3,27 @@ import ImageCard from '../components/imgCard';
 import TextCardA from '../components/txtCardA';
 import TextCardB from '../components/txtCardB';
 import ProductCard from '../components/productCard';
+import ClaimButton from '../components/claimButton';
+import '../css/home.css'
+
 const Home = (props) => {
     return (
         <div>
-            <TextCardA />
-            <ImageCard imgsrc="assets/cat_image1_desktop.png" altlabel="Feeding Cat"/>
-            <ProductCard imgsrc="assets/cat_treats_fd_minnows_01 3.png" altlabel="Cat Treats Minnows" />
-            <ImageCard imgsrc="assets/cat_image2_desktop.png" altlabel="Feeding Cat" />
-            <TextCardB />
+            <section className="Section">
+                <div>
+                    <TextCardA />
+                    <ClaimButton />
+                </div>
+                <ImageCard imgsrc="assets/cat_image1_desktop.png" altlabel="Feeding Cat"/>
+            </section>
+            <section className="Section">
+                <ProductCard imgsrc="assets/cat_treats_fd_minnows_01 3.png" altlabel="Cat Treats Minnows" />
+                <ProductCard imgsrc="assets/cat_treats_fd_minnows_01 3.png" altlabel="Cat Treats Minnows" />
+            </section>
+            <section className="Section">
+                <ImageCard imgsrc="assets/cat_image2_desktop.png" altlabel="Feeding Cat" />
+                <TextCardB />
+            </section>
         </div>
         
 
