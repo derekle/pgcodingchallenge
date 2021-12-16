ba9d309543c2c9df663df1964de6b16e7f8555a0
derek <derekle.creative@gmail.com>
2021-12-15 17:35:18 -0700

    fix: change subheader element from <b> to <h2>

--- public_goods/src/components/txtCardA.js
+++ public_goods/src/components/txtCardA.js
@@ -5,7 +5,7 @@ const TextCardA = (props) => {
         <div className='txtCardA'>
             <h1>Treat your pet (and you).<br/>It's on us!</h1>
             <p>Treat your pet to a healthy treat for <b>free</b> to kick off your Public Goods trial membership</p>
-            <b>See what it's like to be a Public Goods member.</b>
+            <h2>See what it's like to be a Public Goods member.</h2>
             <li>Included in your bundle is a free 2-week membership that gives you unlimited access to our entire collection of sustainable essentials</li>
             <li>Take a load off. We make it easy for you to make better choices. Always included: eco-friendly products you can trust.</li>
         </div>
--- public_goods/src/components/txtCardB.js
+++ public_goods/src/components/txtCardB.js
@@ -4,7 +4,7 @@ const TextCardB = (props) => {
     return (
         <div className='txtCardB'>
             <h1>PET-APPROVED</h1>
-            <b>It feels good to be a member.</b>
+            <h2>It feels good to be a member.</h2>
             <p>After your free trial, become a full-time member with benefits for $59 a year</p>
             <li>Included in your bundle is a free 2-week membership that gives you unlimited access to our entire collection of sustainable essentials</li>
             <li>Take a load off. We make it easy for you to make better choices. Always included: eco-friendly products you can trust.</li>
