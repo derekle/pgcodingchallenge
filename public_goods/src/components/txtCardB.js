69fd5b5d95350e08345987eeb5b460459322b29f
derek <derekle.creative@gmail.com>
2021-12-15 17:27:52 -0700

    new: create second text card component

---
+++ public_goods/src/components/txtCardB.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const TextCardB = (props) => {
+    return (
+        <div className='txtCardB'>
+            <h1>PET-APPROVED</h1>
+            <b>It feels good to be a member.</b>
+            <p>After your free trial, become a full-time member with benefits for $59 a year</p>
+            <li>Included in your bundle is a free 2-week membership that gives you unlimited access to our entire collection of sustainable essentials</li>
+            <li>Take a load off. We make it easy for you to make better choices. Always included: eco-friendly products you can trust.</li>
+        </div>
+
+    )
+}
+
+export default TextCardB
