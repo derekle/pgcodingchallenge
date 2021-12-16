c2fd60751adac5910b9f8d2298be6a4ef72bdf42
derek <derekle.creative@gmail.com>
2021-12-15 17:34:56 -0700

    code: create productcard component

---
+++ public_goods/src/components/productCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = (props) => {
+    return (
+        <div className='productCard'>
+            <h1>OUR TREAT</h1>
+            <img src={require("../" + props.imgsrc)} alt={props.altlabel}></img>
+            <h2>Freeze Dried Minnows</h2>
+            <p>Healthy ingredients with nothing to hide.</p>
+        </div>
+
+    )
+}
+
+export default ProductCard
