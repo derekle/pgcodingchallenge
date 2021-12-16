937a7fae3c59c28582736a24e066486aa2b66f63
derek <derekle.creative@gmail.com>
2021-12-15 17:35:30 -0700

    code: render productcard component

--- public_goods/src/views/Home.js
+++ public_goods/src/views/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ImageCard from '../components/imgCard';
 import TextCardA from '../components/txtCardA';
 import TextCardB from '../components/txtCardB';
+import ProductCard from '../components/productCard';
 const Home = (props) => {
     return (
         <div>
             <TextCardA />
             <ImageCard imgsrc="assets/cat_image1_desktop.png" altlabel="Feeding Cat"/>
-            <ImageCard imgsrc="assets/cat_image2_desktop.png" altlabel="Feeding Cat"/>
+            <ProductCard imgsrc="assets/cat_treats_fd_minnows_01 3.png" altlabel="Cat Treats Minnows" />
+            <ImageCard imgsrc="assets/cat_image2_desktop.png" altlabel="Feeding Cat" />
             <TextCardB />
         </div>
         
