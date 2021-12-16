520dd4332f7381a3a44c9298da002f28d154c515
derek <derekle.creative@gmail.com>
2021-12-15 17:14:18 -0700

    code: render ImageCard components

--- public_goods/src/views/Home.js
+++ public_goods/src/views/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
-
+import ImageCard from '../components/imgCard';
 const Home = (props) => {
-    return <h1>HOME</h1>
+    return (
+        <div>
+            <ImageCard imgsrc="assets/cat_image1_desktop.png" altlabel="Feeding Cat"/>
+            <ImageCard imgsrc="assets/cat_image2_desktop.png" altlabel="Feeding Cat"/>
+        </div>
+        
+
+    )
 };
 
 export default Home
