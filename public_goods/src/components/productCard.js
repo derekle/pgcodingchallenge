0a9db00df7c5a5266171db49c0339c22b5242236
derek <derekle.creative@gmail.com>
2021-12-17 04:43:43 -0700

    code: add container div & subtitle text div

--- public_goods/src/components/productCard.js
+++ public_goods/src/components/productCard.js
@@ -2,11 +2,13 @@ import React from 'react'
 import '../css/productCard.css'
 const ProductCard = (props) => {
     return (
-        <div className='productCard'>
+        <div className='productCard-container'>
             <h1>OUR TREAT</h1>
             <img src={require("../" + props.imgsrc)} alt={props.altlabel}></img>
-            <h2>Freeze Dried Minnows</h2>
-            <p>Healthy ingredients with nothing to hide.</p>
+            <div id="subtext">
+                <h2>Freeze Dried Minnows</h2>
+                <p>Healthy ingredients with nothing to hide.</p>
+            </div>
         </div>
 
     )
