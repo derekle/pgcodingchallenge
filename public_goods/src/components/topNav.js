f9a12d9561aa1ed6f6fbe5141ecdc50b87b8d227
derek <derekle.creative@gmail.com>
2021-12-17 06:56:34 -0700

    code: create topnavbar component and icons

--- public_goods/src/components/topNav.js
+++ public_goods/src/components/topNav.js
@@ -4,3 +4,40 @@ present in the mobile design mockup. I planned to pull some assets from Material
 with the menu and search icons. The cart and item counter would probably be reading from a
 store or be fetching from a backend that would keep info about a user's session.
 */
+
+import React, { Component } from 'react'
+import DragHandleIcon from '@mui/icons-material/DragHandle';
+import SearchIcon from '@mui/icons-material/Search';
+
+import Grid from '@mui/material/Grid';
+import '../css/topNavBar.css'
+
+class TopNav extends Component{
+    render() {
+        return (
+            <div id='topnavbar-container'>
+                <Grid
+                container
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+                >
+                    <Grid item xs={5}>
+                        <DragHandleIcon fontSize='large' />
+                    </Grid>
+                    <Grid item xs={5}>
+                        <h1>PUBLIC <br/> GOODS</h1>
+                    </ Grid>
+                    <Grid item>
+                        <SearchIcon fontSize='medium' />
+                    </Grid>
+                    <Grid item xs="auto">
+                        <span>Cart 0</span>
+                    </Grid>
+                </Grid>
+            </div>
+        )
+    }
+}
+
+export default TopNav
